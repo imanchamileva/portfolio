c56f7f9d04567b1488dc1c8529472fb9d6e18b58
Iman Chamileva <iman.moussaevna@gmail.com>
2022-10-18 20:29:13 +0200

    overlay on project images

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -14,14 +14,25 @@ function Section3() {
 
             <div class="divgeneraleprojets1">
 
-                <a href="https://github.com/imanchamileva/allora" target="_blank"><div className="projet4"><img src={image1} className="projet44" /> </div></a>
+                <div className="firstProject">
+                    <a href="https://github.com/imanchamileva/allora" target="_blank"><div className="projet4"><img src={image1} className="projet44" /> </div></a>
+                    <div className="firstProjectTextOverlay">
+                        <div className="firstProjectText">
+                            HTML CSS JS
+                        </div>
+                    </div>
+                </div>
+
 
                 <a href="https://jdld.interface3.be/" target="_blank"><div className="projet4"><img src={image2} className="projet44" /></div></a>
+
+                <a href="https://coffeewars.interface3.be/" target="_blank"><div className="projet4"><img src={image3} className="projet44" /></div></a>
 
             </div>
 
             <div class="divgeneraleprojets2">
-                <a href="https://coffeewars.interface3.be/" target="_blank"><div className="projet4"><img src={image3} className="projet44" /></div></a>
+
+
 
                 <a href="https://imanchamileva.github.io/project" target="_blank"><div className="projet4"><img src={image4} className="projet44" /></div></a>
 

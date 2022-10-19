198163798fb5196945384554afc83fb79e87a191
Iman Chamileva <iman.moussaevna@gmail.com>
2022-10-19 18:52:10 +0200

    clickable projects links

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -3,8 +3,7 @@ import image1 from "../images/projet1.png"
 import image2 from "../images/projet2.png"
 import image3 from "../images/projet3.png"
 import image4 from "../images/projet4.png"
-import Section2 from './Section2'
-
+import image5 from "../images/projet5.png"
 
 function Section3() {
     return (
@@ -15,31 +14,37 @@ function Section3() {
             <div class="divgeneraleprojets1">
 
                 <div className="firstProject1">
-                    <a href="https://github.com/imanchamileva/allora" target="_blank"><div className="projet4"><img src={image1} className="projet44" /> </div></a>
-                    <div className="firstProjectTextOverlay1">
-                        <div className="firstProjectText1">
-                            HTML CSS JS
+                    <div className="projet4"><img src={image1} className="projet44" /> </div>
+                    <a href="https://github.com/imanchamileva/allora" target="_blank">
+                        <div className="firstProjectTextOverlay1">
+                            <div className="firstProjectText1">
+                                HTML CSS JS
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
 
 
                 <div className="firstProject1">
-                    <a href="https://jdld.interface3.be/" target="_blank"><div className="projet4"><img src={image2} className="projet44" /></div></a>
-                    <div className="firstProjectTextOverlay1">
-                        <div className="firstProjectText1">
-                            TEAM PROJECT : HTML CSS JS PHP
+                    <div className="projet4"><img src={image2} className="projet44" /></div>
+                    <a href="https://jdld.interface3.be/" target="_blank">
+                        <div className="firstProjectTextOverlay1">
+                            <div className="firstProjectText1">
+                                TEAM PROJECT : HTML CSS JS PHP
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
 
                 <div className="firstProject1">
-                    <a href="https://coffeewars.interface3.be/" target="_blank"><div className="projet4"><img src={image3} className="projet44" /></div></a>
-                    <div className="firstProjectTextOverlay1">
-                        <div className="firstProjectText1">
-                            HACKATHON :  HTML CSS JS PHP
+                    <div className="projet4"><img src={image3} className="projet44" /></div>
+                    <a href="https://coffeewars.interface3.be/" target="_blank">
+                        <div className="firstProjectTextOverlay1">
+                            <div className="firstProjectText1">
+                                HACKATHON :  HTML CSS JS PHP
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
 
@@ -47,17 +52,28 @@ function Section3() {
 
 
                 <div className="firstProject1">
-                    <a href="https://imanchamileva.github.io/project" target="_blank"><div className="projet4"><img src={image4} className="projet44" /></div></a>
+                    <div className="projet4"><img src={image4} className="projet44" /></div>
+                    <a href="https://imanchamileva.github.io/project" target="_blank">
+                        <div className="firstProjectTextOverlay1">
+                            <div className="firstProjectText1">
+                                REACT TAILWIND
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="firstProject1">
+                    <a href="https://jdld.interface3.be/" target="_blank"><div className="projet4"><img src={image5} className="projet44" /></div></a>
                     <div className="firstProjectTextOverlay1">
                         <div className="firstProjectText1">
-                            REACT TAILWIND
+                            REACT TYPESCRIPT TAILWIND
                         </div>
                     </div>
                 </div>
-                </div>
-
             </div>
-            )
+
+        </div>
+    )
 }
 
-            export default Section3
+export default Section3
